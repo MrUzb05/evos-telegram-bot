b984cd4f9a30d5b04dcb2790dd6a51f409147d86
Hasan Mirmuhamedov <hasanmirmuhamedov5@gmail.com>
2024-06-13 17:49:51 +0500

    birinchi commit

---
+++ loader.py
@@ -0,0 +1,18 @@
+import logging
+
+from aiogram import Bot, Dispatcher
+from aiogram.types import ParseMode
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+from database.db_sqlite import Database
+
+API_TOKEN = '7041539707:AAFyVMhmB7gEonKdBoPdr0tES8_lPh0Wtzc'
+
+logging.basicConfig(level=logging.INFO)
+
+storage = MemoryStorage()
+
+bot = Bot(token=API_TOKEN, parse_mode = ParseMode.HTML)
+
+dp = Dispatcher(bot, storage=storage)
+db = Database('main.sqlite3')
